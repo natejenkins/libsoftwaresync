f9b8f2316c386fe506de0adea73ca2829a43be44
nathan jenkins <nate.jenkins@gmail.com>
2021-08-05 22:02:39 +0200

    checkout rawOutputSize only if rawOutputSizes is not null

--- app/src/main/java/com/googleresearch/capturesync/MainActivity.java
+++ app/src/main/java/com/googleresearch/capturesync/MainActivity.java
@@ -521,10 +521,10 @@ public class MainActivity extends Activity {
       for (Size s : rawOutputSizes) {
         Log.i(TAG, s.toString());
       }
+      rawImageResolution = Collections.max(Arrays.asList(rawOutputSizes), new CompareSizesByArea());
     } else {
       Log.i(TAG, "Bayer RAW unavailable!");
     }
-    rawImageResolution = Collections.max(Arrays.asList(rawOutputSizes), new CompareSizesByArea());
 
     Size[] yuvOutputSizes = scm.getOutputSizes(ImageFormat.YUV_420_888);
     if (yuvOutputSizes != null) {
